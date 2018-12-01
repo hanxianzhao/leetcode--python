0370d0c9a7571a5e853cde4aec1893ea6d760021
17561916530 <1195354406@qq.com>
2018-12-02 01:57:32 +0800

    第一个错误版本

---
+++ 第一个错误版本.py
@@ -0,0 +1,38 @@
+'''
+你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+示例:
+给定 n = 5，并且 version = 4 是第一个错误的版本。
+调用 isBadVersion(3) -> false
+调用 isBadVersion(5) -> true
+调用 isBadVersion(4) -> true
+所以，4 是第一个错误的版本。
+'''
+
+'''
+普通的二分查找，题目有点骚。看了半天
+超过94.74% 用时24ms
+C语言还是厉害 用时只有4ms
+差距还是有点大啊
+通过	40 ms	python3
+通过	16 ms	java
+通过	24 ms	python
+通过	4 ms	c
+'''
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 1
+        height = n
+        while low <= height:
+            mid = int((low + height) / 2)
+            if isBadVersion(mid):
+                height = mid-1
+            else:
+                low = mid + 1
+        return low
+
